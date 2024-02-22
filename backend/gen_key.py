0c6fd7528f95788d8d862c79c3020c9c74196677
Delilah <ljmc2000@gmail.com>
2024-02-22 23:50:49 +0000

    use redis to store cookie data for security reasons

--- backend/gen_key.py
+++
@@ -1,4 +0,0 @@
-from cryptography import fernet
-
-fernet_key = fernet.Fernet.generate_key().decode()
-print(fernet_key)
--- backend/session_stuff.py
+++ backend/session_stuff.py
@@ -1,8 +1,7 @@
-import base64
+import redis
 from os import environ as ENV
 from aiohttp_session import setup, session_middleware
-from aiohttp_session.cookie_storage import EncryptedCookieStorage
+from aiohttp_session.redis_storage import RedisStorage
 
 async def on_startup(app):
-	secret_key=base64.urlsafe_b64decode(ENV["CHESSBOARDNET_SECRET_KEY"])
-	setup(app, EncryptedCookieStorage(secret_key))
+	setup(app, RedisStorage(redis.asyncio.Redis(), max_age=60*60*24*30))
