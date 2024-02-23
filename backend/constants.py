f6e5a25ca4b53799dc01de2b6dc5a6aac5d02714
Delilah <ljmc2000@gmail.com>
2024-02-23 00:14:05 +0000

    remove some string literals

---
+++ backend/constants.py
@@ -0,0 +1,4 @@
+logged_in="logged_in"
+password="password"
+user_id="user_id"
+username="username"
--- backend/http_stuff.py
+++ backend/http_stuff.py
@@ -1,5 +1,7 @@
-import base64, bcrypt
+import base64, bcrypt, random
 import websocket_stuff
+
+import constants as c
 
 from aiohttp import web
 from aiohttp_session import get_session, new_session
@@ -12,27 +14,27 @@ routes = web.RouteTableDef()
 @routes.get('/api/info')
 async def get_info(request):
 	session = await get_session(request)
-	user_id=session.get("user_id")
+	user_id=session.get(c.user_id)
 
 	return web.json_response({
-		"username":session.get("username"),
-		"user_id":user_id,
-		"logged_in": user_id is not None,
+		c.username:session.get(c.username),
+		c.user_id:user_id,
+		c.logged_in: user_id is not None,
 	})
 
 @routes.post('/api/login')
 async def login(request):
 	session = await new_session(request)
 	data = await request.json()
-	username=data["username"]
-	password=data["password"]
+	username=data[c.username]
+	password=data[c.password]
 
 	async with connection_pool.connection() as db:
 		cur = await db.execute("select user_id, passhash from users where username=%s",(username,))
 		async for user_id, passhash in cur:
 			if bcrypt.checkpw(password.encode(),passhash.encode()):
-				session["user_id"]=user_id
-				session["username"]=username
+				session[c.user_id]=user_id
+				session[c.username]=username
 				return web.Response(status=200)
 		return web.Response(status=401)
 
@@ -40,8 +42,9 @@ async def login(request):
 async def register(request):
 	data = await request.json()
 	user_id=base64.b85encode(random.randbytes(25)).decode()
-	username=data["username"]
-	passhash=bcrypt.hashpw(data["password"].encode(),bcrypt.gensalt()).decode()
+	username=data[c.username]
+	password=data[c.password]
+	passhash=bcrypt.hashpw(password.encode(),bcrypt.gensalt()).decode()
 	async with connection_pool.connection() as db:
 		try:
 			cur = await db.execute("insert into users (user_id, username, passhash) values (%s, %s, %s)",(user_id, username, passhash))
@@ -54,7 +57,7 @@ async def register(request):
 async def get_websocket(request):
 	try:
 		session = await get_session(request)
-		user_id = session["user_id"]
+		user_id = session[c.user_id]
 	except KeyError:
 		return web.Response(status=401)
 
