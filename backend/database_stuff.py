d63ecc67b624136ca5a2f8224a6b25923beff859
Delilah McCormick <ljmc2000@gmail.com>
2024-03-22 22:36:26 +0000

    removed backend. going to start from scratch in node.js

--- backend/database_stuff.py
+++
@@ -1,7 +0,0 @@
-from os import environ as ENV
-from psycopg_pool import AsyncConnectionPool
-
-connection_pool = AsyncConnectionPool(f"host={ENV['CHESSBOARDNET_DATABASE_HOST']} dbname=chessboardnet user=chessboardnet password={ENV['CHESSBOARDNET_DATABASE_PASSWORD']}",open=False)
-
-async def on_startup(app):
-	await connection_pool.open()
