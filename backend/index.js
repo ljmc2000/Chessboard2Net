f1451b1646c2d98414f0537190794982fef5b9d6
Delilah McCormick <ljmc2000@gmail.com>
2024-03-23 18:57:01 +0000

    websocket only accepts connections from logged in users

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import cookie_parser from 'cookie-parser'
 import express from 'express'
 import pg from 'pg'
-import {WebSocketServer} from 'ws'
 
 import login_stuff from './login_stuff.js'
 import websocket_stuff from './websocket_stuff.js'
@@ -9,7 +8,6 @@ import websocket_stuff from './websocket_stuff.js'
 
 const app = express()
 const http_server = app.listen(3000)
-const ws_server = new WebSocketServer({noServer: true})
 const db_pool = new pg.Pool({
   user: 'chessboardnet',
   password: process.env.CHESSBOARDNET_DATABASE_PASSWORD,
@@ -20,5 +18,5 @@ const db_pool = new pg.Pool({
 
 app.use(cookie_parser())
 app.use(express.json())
-login_stuff(app,db_pool)
-websocket_stuff(http_server, ws_server)
+login_stuff(app, db_pool)
+websocket_stuff(http_server, db_pool)
--- backend/websocket_stuff.js
+++ backend/websocket_stuff.js
@@ -1,5 +1,59 @@
-export default (http_server, ws_server) => {
-	http_server.on('upgrade', (request, socket, head) => {
+import {WebSocketServer} from 'ws'
+
+function parse_cookies(request) {
+	var raw = request.headers.cookie
+	var cookies = []
+	for(var cookie of raw.split(`;`)) {
+		let [ name, ...rest] = cookie.split(`=`)
+		name = name?.trim()
+		if (!name)
+			continue
+		const value = rest.join(`=`).trim()
+		if (!value)
+			continue
+		cookies[name] = decodeURIComponent(value)
+	}
+	return cookies
+}
+
+async function verify_client(info, mark_safe)
+{
+	try {
+		var cookies = parse_cookies(info.req)
+		var result = await db_pool.query("select * from users where login_token=$1", [cookies.login_token])
+		if(result.rowCount!=1) {
+			mark_safe(false, 401, 'Unauthorized')
+		}
+		else {
+			mark_safe(true)
+		}
+	}
+	catch (ex) {
+		console.error(ex)
+	}
+}
+
+export default (http_server, db_pool) => {
+	const ws_server = new WebSocketServer({noServer: true,
+		verifyClient: async function(info, mark_safe)
+		{
+			try {
+				var cookies = parse_cookies(info.req)
+				var result = await db_pool.query("select * from users where login_token=$1", [cookies.login_token])
+				if(result.rowCount!=1) {
+					mark_safe(false, 401, 'Unauthorized')
+				}
+				else {
+					mark_safe(true)
+				}
+			}
+			catch (ex) {
+				console.error(ex)
+			}
+		}
+	})
+
+	http_server.on('upgrade', async (request, socket, head) => {
 		socket.on('error', console.error);
 
 		ws_server.handleUpgrade(request, socket, head, (ws) => {
@@ -14,4 +68,6 @@ export default (http_server, ws_server) => {
 			ws.send(JSON.stringify({message: 'There be gold in them thar hills.'}));
 		})
 	})
+
+
 }
