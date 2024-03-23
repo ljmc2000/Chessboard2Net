7594f95fb500f6b36b52f00aaee41e68c501d2d6
Delilah McCormick <ljmc2000@gmail.com>
2024-03-23 00:35:52 +0000

    added websocket handler

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,33 @@
 const express = require('express')
-const app = express()
+const ws = require('ws')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+const app = express()
+const ws_server = new ws.WebSocketServer({noServer: true})
+const http_server = app.listen(3000)
+
+
+http_server.on('upgrade', (request, socket, head) => {
+  socket.on('error', console.error);
+
+  if(request.url==='/api/ws')
+  {
+    ws_server.handleUpgrade(request, socket, head, (ws) => {
+      ws_server.emit('connection', ws, request)
+    })
+  }
 })
 
-app.listen(3000)
+
+ws_server.on('connection',(ws, request, client) => {
+    ws.on('error', console.error);
+
+    ws.on('message', (message) => {
+      ws.send(JSON.stringify({message: 'There be gold in them thar hills.'}));
+    })
+  }
+)
+
+app.get('/api/test', (req, res) => {
+  res.send({working: true})
+})
