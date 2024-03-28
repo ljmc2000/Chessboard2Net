117cd18fce8f4ab0f314fff1c6ed4d709ed77ed3
Delilah McCormick <ljmc2000@gmail.com>
2024-03-28 22:52:22 +0000

    added welcome message and concept of system messages

--- frontend/src/app/components/main/main.component.ts
+++ frontend/src/app/components/main/main.component.ts
@@ -9,6 +9,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { ChessWebsocketHandlerService } from 'services/chess-websocket-handler.service';
 import { WebsocketConsumer } from 'models/websocket-consumer';
 import { ChatMessage } from 'models/chat-message'
+import { ON_JOIN_MESSAGE } from 'constants/standard-messages';
 
 const WhisperPattern = /\/w(?:hisper)? ([^ ]+) (.+)/
 
@@ -21,7 +22,7 @@ const WhisperPattern = /\/w(?:hisper)? ([^ ]+) (.+)/
 })
 export class MainComponent implements WebsocketConsumer {
   chatMessageContent: string='';
-  chatLog: ChatMessage[] = [];
+  chatLog: ChatMessage[] = [ON_JOIN_MESSAGE];
 
   constructor(private ws: ChessWebsocketHandlerService) {
     ws.subscribeToWS(this);
---
+++ frontend/src/app/constants/standard-messages.ts
@@ -0,0 +1,3 @@
+import { ChatMessage } from 'models/chat-message';
+
+export const ON_JOIN_MESSAGE: ChatMessage = {content: "Welcome to Chessboard2Net. Type /h for a list of commands or say hello and see if someone challenges you to a chess game."} as ChatMessage;
