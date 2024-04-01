3b75432cd02199c49b0466e06014f6dd9dc92cb2
Delilah McCormick <ljmc2000@gmail.com>
2024-04-01 22:08:05 +0100

    added settings component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from 'components/main/main.component';
 import { LoginComponent } from 'components/login/login.component';
+import { SettingsComponent } from 'components/settings/settings.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'settings', component: SettingsComponent },
 ];
