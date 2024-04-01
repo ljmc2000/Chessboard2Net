5d769c8fcfa73bdc4357fdd84541fbbb01644ddb
Delilah McCormick <ljmc2000@gmail.com>
2024-04-01 23:49:43 +0100

    some progress on settings page

--- frontend/src/app/components/settings/settings.component.ts
+++ frontend/src/app/components/settings/settings.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+import { ChessWebsocketHandlerService } from 'services/chess-websocket-handler.service';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatCheckboxModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
+  self_visiblity: boolean;
 
+  constructor(private ws: ChessWebsocketHandlerService) {
+  }
+
+  onChangePrivacy() {
+  }
 }
