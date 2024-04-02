e4f720fb904abbfa7fe88107e9c52f79ace9c2ee
Delilah McCormick <ljmc2000@gmail.com>
2024-04-02 18:23:19 +0100

    no space in usernames constraint use \s not space character

--- sql/setup.sql
+++ sql/setup.sql
@@ -5,7 +5,7 @@ create table users (
 	login_token character(48),
 	login_expires timestamp,
 
-	constraint no_spaces_in_usernames check (username !~ ' ')
+	constraint no_spaces_in_usernames check (username !~ '\s')
 );
 
 create unique index case_insensitive_usernames on users (upper(username));
