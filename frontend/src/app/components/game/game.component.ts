eec059fabd193bdf7ad123835b393ba2d89eed6d
Delilah McCormick <ljmc2000@gmail.com>
2024-04-06 22:03:11 +0100

    apply colour to chess pieces in the game view

--- frontend/src/app/components/game/game.component.ts
+++ frontend/src/app/components/game/game.component.ts
@@ -1,14 +1,15 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChildren, QueryList } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIcon } from '@angular/material/icon';
 
 import { ChessWebsocketHandlerService } from 'services/chess-websocket-handler.service';
 import { GameState } from 'models/gamestate'
 import { PlayerInfo } from 'models/playerinfo'
 import * as S from 'shared/chess-sets'
 import * as I from 'shared/instructions';
+import { parse_colour, waitForElm } from 'utils';
 
 @Component({
   selector: 'app-game',
@@ -19,14 +20,18 @@ import * as I from 'shared/instructions';
 })
 export class GameComponent {
 
-  IS_CAP=/[A-Z]/
+  IS_PLAYER1=/[A-Z]/
+  IS_PLAYER2=/[a-z]/
 
   in_game: boolean=false;
   is_player1: boolean=true;
   gamestate: string='';
   icon_map: any={};
 
-  //@ViewChild('board') board: ElementRef;
+  player1_colour: string='white';
+  player2_colour: string='black';
+
+  @ViewChildren('chess_square') board: QueryList<MatIcon>;
 
   constructor(public ws: ChessWebsocketHandlerService) {
     ws.on(I.GOVER, ()=>this.in_game=false);
@@ -34,11 +39,20 @@ export class GameComponent {
     ws.on(I.PINF, (msg: PlayerInfo)=>this.onPlayerInfo(msg));
   }
 
+  ngAfterViewInit() {
+    this.board.changes.subscribe((icons: QueryList<MatIcon>)=>this.afterUpdateGamestate(icons))
+  }
+
   invertBoard(gamestate: string) {
     return gamestate.split('').reverse().join('')
   }
 
   onPlayerInfo(msg: PlayerInfo) {
+    if(msg.is_player1)
+      this.player1_colour=parse_colour(msg.favourite_colour)
+    else
+      this.player2_colour=parse_colour(msg.favourite_colour)
+
     var c_set='doodles/';
     switch(msg.prefered_set) {
       case S.DOODLES:
@@ -96,5 +110,21 @@ export class GameComponent {
       this.gamestate=msg.gamestate;
     else
       this.gamestate=this.invertBoard(msg.gamestate)
+  }
+
+  async afterUpdateGamestate(icons: QueryList<MatIcon>) {
+    for(var icon of icons) {
+      waitForElm(icon._elementRef.nativeElement,'.player1 .custom_colour')
+      .then((list)=>list.forEach(area=>{
+          area.style.fill=this.player1_colour
+      }))
+    }
+
+    for(var icon of icons) {
+      waitForElm(icon._elementRef.nativeElement,'.player2 .custom_colour')
+      .then((list)=>list.forEach(area=>{
+        area.style.fill=this.player2_colour
+      }))
+    }
   }
 }
--- frontend/src/app/utils.ts
+++ frontend/src/app/utils.ts
@@ -1,3 +1,24 @@
 export function parse_colour(colour: number): string {
   return '#'+colour.toString(16).padStart(6,'0')
 }
+
+export function waitForElm(e: HTMLElement, selector: string): Promise<NodeListOf<HTMLElement>> {
+  return new Promise(resolve => {
+    if (e.querySelector(selector)) {
+      return resolve(e.querySelectorAll<HTMLElement>(selector));
+    }
+
+    const observer = new MutationObserver(mutations => {
+      if (e.querySelector(selector)) {
+        observer.disconnect();
+        return resolve(e.querySelectorAll<HTMLElement>(selector));
+      }
+    });
+
+    // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+    observer.observe(e, {
+      childList: true,
+      subtree: true
+    });
+  });
+}
