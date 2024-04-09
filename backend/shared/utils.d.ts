3474937571a4b226abf675736eae975fb275aa12
Delilah McCormick <ljmc2000@gmail.com>
2024-04-09 20:06:13 +0100

    added function to generate algerbraic names

--- backend/shared/utils.d.ts
+++ backend/shared/utils.d.ts
@@ -1,1 +1,2 @@
+export function generate_algerbraic_names(): string[];
 export function owner(piece: string): number;
--- backend/shared/utils.js
+++ backend/shared/utils.js
@@ -3,6 +3,17 @@ import { PlayerNumber } from './constants.js'
 const IS_PLAYER1=/[A-Z]/
 const IS_PLAYER2=/[a-z]/
 
+export function generate_algerbraic_names() {
+	const x_names="ABCDEFGH"
+	var rainbow_table = []
+	for(var y=8; y>=0; y--) {
+		for(var x=0; x<8; x++) {
+			rainbow_table.push(`${x_names[x]}${y}`)
+		}
+	}
+	return rainbow_table
+}
+
 export function owner(piece) {
 	if(IS_PLAYER1.exec(piece))
 		return PlayerNumber.ONE
