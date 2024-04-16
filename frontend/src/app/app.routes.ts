9bb13dc7103389f64c7ee4ed8a939a1fa2a2dc1e
Delilah McCormick <ljmc2000@gmail.com>
2024-04-16 19:15:06 +0100

    added lobby to charms

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { ChatComponent } from 'components/chat/chat.component';
 import { GameComponent } from 'components/game/game.component';
+import { LobbyComponent } from 'components/lobby/lobby.component';
 import { LoginComponent } from 'components/login/login.component';
 import { SettingsComponent } from 'components/settings/settings.component';
 
 export const routes: Routes = [
   { path: 'chat', component: ChatComponent },
   { path: 'game', component: GameComponent },
+  { path: 'lobby', component: LobbyComponent },
   { path: 'login', component: LoginComponent },
   { path: 'settings', component: SettingsComponent },
   { path: '**', component: ChatComponent },
