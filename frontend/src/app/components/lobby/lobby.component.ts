40e95897c4df262236bdf177e567ab4a04f99358
Delilah McCormick <ljmc2000@gmail.com>
2024-04-17 14:52:17 +0100

    users in lobby look even slightly cool

--- frontend/src/app/components/lobby/lobby.component.ts
+++ frontend/src/app/components/lobby/lobby.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatIconModule } from '@angular/material/icon';
+
 import { ChessWebsocketHandlerService } from 'services/chess-websocket-handler.service';
 import { Instruction as I, UserEvent } from 'shared/constants';
+import { PieceColourService } from 'services/piece-colour.service';
 import { UserInfo } from 'models/user-info';
+import { parse_colour, set_for } from 'utils';
 
 @Component({
   selector: 'app-lobby',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [ CommonModule, MatIconModule ],
   templateUrl: './lobby.component.html',
   styleUrl: './lobby.component.css'
 })
@@ -16,7 +20,10 @@ export class LobbyComponent {
 
   online_users: Map<string,UserInfo>=new Map();
 
-  constructor(public ws: ChessWebsocketHandlerService) {
+  constructor(
+    private ws: ChessWebsocketHandlerService,
+    private colourService: PieceColourService,
+  ) {
     ws.on(I.READY, (data: any)=>this.ws.subscribeToUserEvents());
     ws.on(I.UENV,(message: any)=>this.onUserEvent(message.ev, message.sender));
   }
@@ -27,6 +34,11 @@ export class LobbyComponent {
     }
     else {
       this.online_users.set(user.user_id, user);
+      this.colourService.setColour(user.favourite_colour.toString(16), 'custom_colour', parse_colour(user.favourite_colour));
     }
+  }
+
+  iconFor(set_id: number): string {
+    return set_for(set_id)+"/pawn";
   }
 }
