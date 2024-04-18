b1c9e59151949d2049f3a904959884375d2c330c
Delilah McCormick <ljmc2000@gmail.com>
2024-04-18 15:59:58 +0100

    changed way challenges are issued in the lobby

--- frontend/src/app/components/lobby/lobby.component.ts
+++ frontend/src/app/components/lobby/lobby.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 
 import { ChatComponent } from 'components/chat/chat.component';
@@ -20,16 +21,29 @@ import { parse_colour, set_for } from 'utils';
 })
 export class LobbyComponent {
 
-  self: UserInfo={} as UserInfo;
+  challenge_username: string;
   online_users: Map<string,UserInfo>=new Map();
+  self: UserInfo={} as UserInfo;
 
   constructor(
     public ws: ChessWebsocketHandlerService,
     private colourService: PieceColourService,
+    private dialog: MatDialog,
   ) {
     ws.on(I.SINF, (user: UserInfo)=>this.self=user);
     ws.on(I.READY, (data: any)=>this.ws.subscribeToUserEvents());
     ws.on(I.UENV,(message: any)=>this.onUserEvent(message.ev, message.sender));
+  }
+
+  challenge(username: string) {
+    this.challenge_username=username;
+    const dialogRef = this.dialog.open(ChallengeDialog, {
+      data: {username: this.challenge_username}
+    });
+
+    dialogRef.afterClosed().subscribe(game => {
+      this.ws.challenge(username, game);
+    });
   }
 
   onSinf(user: UserInfo) {
@@ -52,4 +66,26 @@ export class LobbyComponent {
   iconFor(set_id: number): string {
     return set_for(set_id)+"/pawn";
   }
+}
+
+@Component({
+  selector: 'challenge-dialog',
+  standalone: true,
+  imports: [ CommonModule, MatButtonModule, MatDialogModule ],
+  templateUrl: './lobby-challenge-dialog.html',
+  styleUrl: './lobby.component.css'
+})
+class ChallengeDialog {
+  constructor(
+    public dialogRef: MatDialogRef<ChallengeDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: ChallengeData,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
+
+interface ChallengeData {
+  username: string;
 }
